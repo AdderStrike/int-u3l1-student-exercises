cb1c1c9eece71fe2d4e68f399407b41699c28ded
AdderStrike <chemeng133@gmail.com>
2025-06-11 11:09:52 -0500

    Finished JS

--- variables.js
+++ variables.js
@@ -2,10 +2,10 @@
 let favoriteColor;
 
 // 1. Create a variable called `favoriteFood`.
-
+let favoriteFood;
 
 // 2. Create a variable called `favoriteNumber`.
-
+let favoriteNumber;
 
 
 
@@ -14,5 +14,12 @@ let favoriteColor;
 // - Use a string value of your favorite color.
 // - Use a string value of your favorite food.
 // - Use a number value of your favorite number.
+favoriteColor = "blue";
+favoriteFood = "General Tso's Chicken";
+favoriteNumber = Number.MIN_SAFE_INTEGER;
+
 
 // 4. Console log each variable on a new line.
+console.log("Favorite Color: "+favoriteColor);
+console.log("Favorite Food: "+favoriteFood);
+console.log("Favorite Number: "+favoriteNumber);
